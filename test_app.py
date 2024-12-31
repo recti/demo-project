154f997ff60d82c35fd9ba2d96ada39f9e40bc80
anass <anass.nasserallah@gmail.com>
2024-12-31 22:29:58 +0100

    tests - artifacts added

--- test_app.py
+++ test_app.py
@@ -4,7 +4,11 @@ from app import add, sub
 # Test function for add
 def test_add():
     assert add(2, 3) == 5
+    assert add(-1, 1) == 0
+    assert add(0, 0) == 0
 
 # Test function for sub
 def test_sub():
     assert sub(3, 2) == 1
+    assert sub(10, 5) == 5
+    assert sub(0, 0) == 0
